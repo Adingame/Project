91f28fa7240ab5229a5fbc8c357cd3b7c950dbe6
TikTaq'to <adai.02_kz@mail.ru>
2020-06-24 20:57:17 +0600

    User

---
+++ TestWork/AuthorizationAndRegistration.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestWork
+{
+    static class AuthorizationAndRegistration
+    {
+        public static void checkIn(User[] authorizationsUsers)
+        {
+            string fullName, email, password, phoneNumber;
+
+            string reenterPassword;
+
+            bool passwordChecker = false;
+
+            Console.WriteLine("Enter your full name: ");
+            fullName = Console.ReadLine();
+
+            Console.WriteLine("Enter your email: ");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Enter your password: ");
+            password = Console.ReadLine();
+
+            Console.WriteLine("Enter your phone number: ");
+            phoneNumber = Console.ReadLine();
+
+            while (!passwordChecker)
+            {
+                Console.WriteLine("Re-enter Password");
+                reenterPassword = Console.ReadLine();
+                if (reenterPassword == password)
+                {
+                    Console.WriteLine("Ok, you registered");
+                    passwordChecker = true;
+                }
+            }
+
+            var newUser = new User(fullName, phoneNumber, email, password);
+
+        }
+
+        public static bool LoginToAccount(User userAccount, string password, string login)
+        {
+            if (userAccount.Email == login && userAccount.Password == password)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ TestWork/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestWork
+{
+    public class User
+    {
+        public string FullName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public User(string fullName, string phoneNumber, string email, string password)
+        {
+            FullName = fullName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Password = password;
+        }
+    }
+}
